274ff8ed980293dbf9fdf145a6c93bc5bd698f37
shahin2525 <shahin451999@gmail.com>
2024-11-28 22:29:23 -0800

    add mongoose

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -6,4 +6,6 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
   port: process.env.PORT,
+  db_url: process.env.DB_URL,
+  salt: process.env.SALT,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 /* eslint-disable prettier/prettier */
+import mongoose from 'mongoose'
 import app from './app'
 import config from './app/config'
 
-app.listen(config.port, () => {
-  console.log(`Example app listening on port ${config.port}`)
-})
+async function main() {
+  try {
+    await mongoose.connect(config.db_url as string)
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
